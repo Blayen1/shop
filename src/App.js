ac73ce52eea5f100cc39a0a82d4764906f33eb59
Blayen1 <blayen@bk.ru>
2022-12-15 23:59:35 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Items from './components/Items';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends  React.Component {
+
+constructor(props){
+super(props)
+this.state = {
+  items:[
+    {
+    id:1,
+    title:'Стул серый',
+    img:'grey-chair.jpg',
+    desc:'Do laboris consequat ipsum minim et amet officia deserunt.',
+    category:'chairs',
+    price:'49.99'
+  },   {
+    id:2,
+    title:'Стол',
+    img:'table.jpg',
+    desc:'Do laboris consequat ipsum minim et amet officia deserunt.',
+    category:'table',
+    price:'49.99'
+  },   {
+    id:3,
+    title:'Диван ',
+    img:'sofa.jpg',
+    desc:'Do laboris consequat ipsum minim et amet officia deserunt.',
+    category:'sofa',
+    price:'549.99'
+  },   {
+    id:4,
+    title:'Лампа ',
+    img:'lamp.jpg',
+    desc:'Do laboris consequat ipsum minim et amet officia deserunt.',
+    category:'lamp',
+    price:'100.99'
+  },   {
+    id:5,
+    title:'Стул белый',
+    img:'white-chair.jpg',
+    desc:'Do laboris consequat ipsum minim et amet officia deserunt.',
+    category:'chair',
+    price:'49.99'
+  }, {
+    id:6,
+    title:'Диван ',
+    img:'sofa.jpg',
+    desc:'Do laboris consequat ipsum minim et amet officia deserunt.',
+    category:'sofa',
+    price:'549.99'
+  } 
+]
+}
+}
+  render() {
+    return (
+      <div className="wrapper">
+        <Header/>
+        <Items  items={this.state.items} />
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
